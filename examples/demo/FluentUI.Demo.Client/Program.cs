92c435e3442d786f2dbbcc25d650580298aaa55a
Marvin Klein <support@marvinkleinmusic.de>
2025-05-29 00:11:51 +0200

    Add sample projects

---
+++ examples/demo/FluentUI.Demo.Client/Program.cs
@@ -0,0 +1,17 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+using FluentUI.Demo.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents();
+
+await builder.Build().RunAsync();
