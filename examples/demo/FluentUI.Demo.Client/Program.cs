5fbbc5f1b3aef5390e6080ab6888434d270569dc
Jérémy JANISZEWSKI <jeremy@act-motion.net>
2025-06-19 08:29:56 +0200

    adds some settings to make AIPrompt work Update program.cs to add the good scoped services for FileManager working

--- examples/demo/FluentUI.Demo.Client/Program.cs
+++ examples/demo/FluentUI.Demo.Client/Program.cs
@@ -2,8 +2,10 @@
 // MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
 // ------------------------------------------------------------------------
 
+using FluentUI.Blazor.Community.Components;
 using FluentUI.Blazor.Community.Extensions;
 using FluentUI.Demo.Client;
+using FluentUI.Demo.Shared.Providers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
@@ -15,7 +17,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
 builder.Services.AddFluentCxUIComponents();
-//builder.Services.AddSingleton<IFileManagerItemsProvider<NoFileEntryData>, FileManagerItemsProvider>();
-//builder.Services.AddScoped<FileManagerSortState>();
+builder.Services.AddAzureOpenAI();
+builder.Services.AddSingleton<IFileManagerItemsProvider<NoFileEntryData>, FileManagerItemsProvider>();
+builder.Services.AddScoped<FileManagerState>();
 
 await builder.Build().RunAsync();
--- examples/demo/FluentUI.Demo.Shared/Providers/FileManagerItemsProvider.cs
+++ examples/demo/FluentUI.Demo.Shared/Providers/FileManagerItemsProvider.cs
@@ -9,22 +9,9 @@ namespace FluentUI.Demo.Shared.Providers;
 public class FileManagerItemsProvider
     : IFileManagerItemsProvider<NoFileEntryData>
 {
-    private static async ValueTask<FileManagerEntry<NoFileEntryData>> BuildAsync()
-    {
-        var entry = FileManagerEntry<NoFileEntryData>.Home;
-        //var bookEntry = entry.CreateDirectory("Books");
-        //var documentsEntry = entry.CreateDirectory("Documents");
-        //var journeyEntry = entry.CreateDirectory("Journey");
-        //var powerbiEntry = entry.CreateDirectory("Powerbi");
-
-
-        return await ValueTask.FromResult(entry);
-    }
-
-
-
     public ValueTask<FileManagerEntry<NoFileEntryData>> GetItemsAsync()
     {
-        return BuildAsync();
+        var entry = FileManagerEntry<NoFileEntryData>.Home;
+        return ValueTask.FromResult(entry);
     }
 }
