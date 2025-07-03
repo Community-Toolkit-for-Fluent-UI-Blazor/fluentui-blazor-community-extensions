c00dfa61f34248adb695bd8531e684c8a56b7164
Jérémy JANISZEWSKI <jeremy@act-motion.net>
2025-07-03 14:10:22 +0200

    adds a grid layout on the fluentcxdropzonecontainer adds a tile grid layout for the fluentcxtilegrid

--- src/Community.Components/Components/TileGrid/FluentCxTileGrid.cs
+++ src/Community.Components/Components/TileGrid/FluentCxTileGrid.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.CompilerServices;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace FluentUI.Blazor.Community.Components;
 
@@ -13,14 +14,30 @@ namespace FluentUI.Blazor.Community.Components;
 /// <typeparam name="TItem">Type of the component.</typeparam>
 [CascadingTypeParameter(nameof(TItem))]
 public class FluentCxTileGrid<TItem>
-    : FluentComponentBase
+    : FluentComponentBase, IDisposable
 {
+    /// <summary>
+    /// Represents the layout of the tile grid.
+    /// </summary>
+    private readonly TileGridLayout _layout = [];
+
+    /// <summary>
+    /// Represents the javascript file to use to load and to store the layout.
+    /// </summary>
+    private const string JavascriptFile = "./_content/FluentUI.Blazor.Community.Components/js/TileGridLayout.js";
+
+    /// <summary>
+    /// Represents the reference of the javascript object. 
+    /// </summary>
+    private IJSObjectReference? _module;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="FluentCxTileGrid{TItem}"/> component.
     /// </summary>
     public FluentCxTileGrid() : base()
     {
         Id = Identifier.NewId();
+        _layout.SaveRequested += OnSaveRequested;
     }
 
     /// <summary>
@@ -36,6 +53,9 @@ public class FluentCxTileGrid<TItem>
         Width = Width,
         MinimumRowHeight = MinimumRowHeight
     };
+
+    [Inject]
+    private IJSRuntime? JSRuntime { get; set; }
 
     /// <summary>
     /// Gets the <see cref="FluentCxDropZoneContainer{TItem}"/> component.
@@ -154,6 +174,113 @@ public class FluentCxTileGrid<TItem>
     [Parameter]
     public string? Height { get; set; } = "100%";
 
+    /// <summary>
+    /// Gets or sets a value indicating if the layout will be stored in the local storage of the app or not.
+    /// </summary>
+    /// <remarks>
+    /// When this value is set to <see langword="true" />, the <see cref="FluentComponentBase.Id"/> property must be set
+    ///  and mustn't change.</remarks>
+    [Parameter]
+    public bool PersistenceEnabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets the function to extract a key from an item.
+    /// </summary>
+    /// <remarks>This function is only used if <see cref="PersistenceEnabled"/> is set to <see langword="true" />.</remarks>
+    [Parameter]
+    public Func<TItem, string>? ItemKey { get; set; }
+
+    /// <summary>
+    /// Occurs when a save is requested.
+    /// </summary>
+    /// <param name="sender">Object who invokes the method.</param>
+    /// <param name="e">Event args associated to this event.</param>
+    private async void OnSaveRequested(object? sender, EventArgs e)
+    {
+        await SaveLayoutAsync();
+    }
+
+    /// <summary>
+    /// Adds a layout item.
+    /// </summary>
+    /// <param name="value">Item to add.</param>
+    /// <param name="index">Index of the item.</param>
+    internal void AddLayoutItem(FluentCxTileGridItem<TItem>? value, int? index)
+    {
+        if (index.HasValue && value is not null && ItemKey is not null)
+        {
+            _layout.Add<TileGridLayoutItem>(ItemKey(value.Value), index.Value);
+        }
+    }
+
+    /// <summary>
+    /// Remove a layout item.
+    /// </summary>
+    /// <param name="index">Index of the item to remove.</param>
+    internal void RemoveLayoutItem(int? index)
+    {
+        if (index.HasValue)
+        {
+            _layout.Remove(index.Value);
+        }
+    }
+
+    /// <summary>
+    /// Update the layout in an asynchronous way.
+    /// </summary>
+    /// <param name="value">Item to update.</param>
+    /// <returns></returns>
+    internal async Task UpdateLayoutAsync(FluentCxTileGridItem<TItem> value)
+    {
+        if (ItemKey is not null)
+        {
+            _layout.UpdateSpan(ItemKey(value.Value), value.ColumnSpan, value.RowSpan);
+            await SaveLayoutAsync();
+        }
+    }
+
+    /// <summary>
+    /// Save the layout in an asynchronous way.
+    /// </summary>
+    /// <returns>Returns a task which saves the layout when completed.</returns>
+    private async Task SaveLayoutAsync()
+    {
+        if (PersistenceEnabled &&
+            _layout is not null &&
+            _module is not null &&
+            !string.IsNullOrEmpty(Id))
+        {
+            await _module.InvokeVoidAsync(
+                "saveLayout", Id, _layout.Items);
+        }
+    }
+
+    /// <inheritdoc />
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (PersistenceEnabled && ItemKey is null)
+        {
+            throw new InvalidOperationException("When PersistenceEnabled is set to true, ItemKey must be defined.");
+        }
+    }
+
+    /// <inheritdoc />
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender &&
+            JSRuntime is not null &&
+            PersistenceEnabled)
+        {
+            _module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", JavascriptFile);
+            _layout.AddRange(await _module.InvokeAsync<IEnumerable<TileGridLayoutItem>>("loadLayout", Id));
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+
     /// <inheritdoc />
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
@@ -177,7 +304,9 @@ public class FluentCxTileGrid<TItem>
             __builder2.AddComponentParameter(14, nameof(FluentCxDropZoneContainer<TItem>.ItemContent), ItemContent);
             __builder2.AddComponentParameter(15, nameof(FluentCxDropZoneContainer<TItem>.TileGridSettings), GridSettings);
             __builder2.AddComponentParameter(16, nameof(FluentCxDropZoneContainer<TItem>.CanOverflow), CanOverflow);
-            __builder2.AddComponentReferenceCapture(17, (__value) =>
+            __builder2.AddComponentParameter(17, nameof(FluentCxDropZoneContainer<TItem>.Layout), _layout);
+            __builder2.AddComponentParameter(18, nameof(FluentCxDropZoneContainer<TItem>.ItemKey), ItemKey);
+            __builder2.AddComponentReferenceCapture(19, (__value) =>
             {
                 _dropContainer = (FluentCxDropZoneContainer<TItem>)__value;
             }
@@ -186,5 +315,12 @@ public class FluentCxTileGrid<TItem>
         });
 
         builder.CloseComponent();
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _layout.SaveRequested -= OnSaveRequested;
+        GC.SuppressFinalize(this);
     }
 }
