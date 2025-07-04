b5cc83bb707fc90eac396ef473c119b3ef0a4183
Marvin Klein <32510006+MarvinKlein1508@users.noreply.github.com>
2025-07-04 12:25:44 +0200

    Revert "Add many summaries"

--- src/Community.Components/Components/FileManager/FileManagerState.cs
+++ src/Community.Components/Components/FileManager/FileManagerState.cs
@@ -1,38 +1,14 @@
 namespace FluentUI.Blazor.Community.Components;
 
-/// <summary>
-/// Represents the state of the <see cref="FluentCxFileManager{TItem}"/>.
-/// </summary>
 public sealed class FileManagerState
 {
-    /// <summary>
-    /// Represents the way the files are sorted by.
-    /// </summary>
     private FileSortBy _sortBy;
-
-    /// <summary>
-    /// Represents the ascending or descending sort.
-    /// </summary>
     private FileSortMode _sortMode;
-
-    /// <summary>
-    /// Represents the current view of the file manager.
-    /// </summary>
     private FileView _view;
 
-    /// <summary>
-    /// Gets or sets the event to raise when the sort is updated.
-    /// </summary>
     public event EventHandler? OnSortUpdated;
-
-    /// <summary>
-    /// Gets or sets the event to raise when the view is updated.
-    /// </summary>
     public event EventHandler? OnViewUpdated;
 
-    /// <summary>
-    /// Gets or sets the way the files are sorted.
-    /// </summary>
     public FileSortBy SortBy
     {
         get => _sortBy;
@@ -47,9 +23,6 @@ public sealed class FileManagerState
         }
     }
 
-    /// <summary>
-    /// Gets the sorted mode of the files (ascending or descending).
-    /// </summary>
     public FileSortMode SortMode
     {
         get => _sortMode;
@@ -63,9 +36,6 @@ public sealed class FileManagerState
         }
     }
 
-    /// <summary>
-    /// Gets the view of the file manager.
-    /// </summary>
     public FileView View
     {
         get => _view;
@@ -79,3 +49,4 @@ public sealed class FileManagerState
         }
     }
 }
+
