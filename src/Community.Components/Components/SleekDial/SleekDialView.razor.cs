131cea71d02f44df8fec78ebc0786c15837bda7f
Jérémy JANISZEWSKI <jeremy@act-motion.net>
2025-08-23 19:37:51 +0200

    Enhance the FluentCxSleekDial component... Add a StayOpen parameter to allow the component to stay open when an item is clicked Adds a IsVisible parameter to show or collapse the item Correct an issue with a CSS

--- src/Community.Components/Components/SleekDial/SleekDialView.razor.cs
+++ src/Community.Components/Components/SleekDial/SleekDialView.razor.cs
@@ -3,9 +3,36 @@ using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace FluentUI.Blazor.Community.Components;
 
+/// <summary>
+/// Represents the viewer of a <see cref="SleekDialItem"/> collection.
+/// </summary>
 public partial class SleekDialView
     : FluentComponentBase
 {
+    /// <summary>
+    /// Represents the <see cref="FluentCxSleekDial"/> component.
+    /// </summary>
     [CascadingParameter]
     internal FluentCxSleekDial Parent { get; set; } = default!;
+
+    /// <summary>
+    /// Refreshes the component state.
+    /// </summary>
+    internal void Refresh()
+    {
+        StateHasChanged();
+    }
+
+    /// <inheritdoc />
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        if (Parent is null)
+        {
+            throw new InvalidOperationException($"{GetType()} must be used inside a {nameof(FluentCxSleekDial)} component.");
+        }
+
+        Parent.Viewer = this;
+    }
 }
