3682bcab2f6dc951bc3d265e75eea3f9d3b5c132
Jérémy JANISZEWSKI <jeremy@act-motion.net>
2025-08-23 08:52:38 +0200

    add some unit tests

---
+++ tests/FluentUI.Blazor.Community.Components.Tests/Extensions/RenderTreeBuilderExtensionsTests.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentUI.Blazor.Community.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace FluentUI.Blazor.Community.Components.Tests.Extensions;
+
+public class RenderTreeBuilderExtensionsTests
+{
+    [Fact]
+    public void AddChildContent_Should_Add_ChildContent_Parameter()
+    {
+        // Arrange
+        var builder = new RenderTreeBuilder();
+        var sequence = 123;
+        RenderFragment fragment = b => b.AddContent(0, "Test content");
+
+        // Act
+        builder.AddChildContent(sequence, fragment);
+
+        // Assert
+        var frames = builder.GetFrames().Array;
+        Assert.Contains(frames, f =>
+            f.FrameType == RenderTreeFrameType.Attribute &&
+            f.AttributeName == "ChildContent" &&
+            f.AttributeValue == fragment
+        );
+    }
+
+    [Fact]
+    public void AddChildContent_With_Null_RenderFragment_Should_Add_Null_ChildContent()
+    {
+        // Arrange
+        var builder = new RenderTreeBuilder();
+        var sequence = 42;
+        RenderFragment fragment = null;
+
+        // Act
+        builder.AddChildContent(sequence, fragment);
+
+        // Assert
+        var frames = builder.GetFrames().Array;
+        Assert.Contains(frames, f =>
+            f.FrameType == RenderTreeFrameType.Attribute &&
+            f.AttributeName == "ChildContent" &&
+            f.AttributeValue == null
+        );
+    }
+
+    [Fact]
+    public void AddChildContent_With_Different_Sequence_Should_Respect_Sequence()
+    {
+        // Arrange
+        var builder = new RenderTreeBuilder();
+        var sequence = 999;
+        RenderFragment fragment = b => b.AddContent(0, "Another content");
+
+        // Act
+        builder.AddChildContent(sequence, fragment);
+
+        // Assert
+        var frames = builder.GetFrames().Array;
+        Assert.Contains(frames, f =>
+            f.FrameType == RenderTreeFrameType.Attribute &&
+            f.Sequence == sequence &&
+            f.AttributeName == "ChildContent"
+        );
+    }
+}
