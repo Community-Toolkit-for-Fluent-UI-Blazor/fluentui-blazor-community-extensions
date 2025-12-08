958162c42a08ce3b84cf99121fc11a2b5b668474
MarvinKlein1508 <support@marvinkleinmusic.de>
2025-12-08 12:54:34 +0100

    More fixes

--- src/Core/Extensions/EnumExtensions.cs
+++ src/Core/Extensions/EnumExtensions.cs
@@ -11,15 +11,6 @@ public static class EnumExtensions
     /// <typeparam name="T">The enum type.</typeparam>
     /// <param name="value">The enum instance to test.</param>
     /// <param name="values">A list of candidate values to compare against.</param>
-    /// <returns>
-    /// <see langword="true"/> if <paramref name="value"/> matches any element in <paramref name="values"/>; otherwise, <see langword="false"/>.
-    /// If no <paramref name="values"/> are supplied the method returns <see langword="false"/>.
-    /// </returns>
-    /// <example>
-    /// <code>
-    /// var result = myEnumValue.IsOneOf(MyEnum.OptionA, MyEnum.OptionB);
-    /// </code>
-    /// </example>
     public static bool IsOneOf<T>(this T value, params T[] values) where T : Enum
     {
         return values.Any(x => Equals(value, x));
