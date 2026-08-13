46814e1eb5f82e833981ead96db5ff8411ce4a97
Marvin Klein <support@marvinkleinmusic.de>
2026-08-13 14:31:34 +0200

    Icons

--- src/Core/Components/SlideShow/FluentCxSlideShow.razor.cs
+++ src/Core/Components/SlideShow/FluentCxSlideShow.razor.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components.Utilities;
 
@@ -9,9 +10,33 @@ namespace FluentUI.Blazor.Community.Components;
 public partial class FluentCxSlideShow : FluentComponentBase
 {
 
+    /// <summary>
+    /// Gets or sets a value indicating that the controls are shown.
+    /// </summary>
+    [Parameter]
+    public bool ShowControls { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the position of the indicator.
+    /// </summary>
+    [Parameter]
+    public SlideShowControlPosition ControlPosition { get; set; } = SlideShowControlPosition.Bottom;
+
+    /// <summary>
+    /// Gets or sets the previous icon.
+    /// </summary>
+    [Parameter]
+    public Icon PreviousIcon { get; set; } = new Icons.Regular.Size24.ChevronLeft();
+
+    /// <summary>
+    /// Gets or sets the next icon.
+    /// </summary>
+    [Parameter]
+    public Icon NextIcon { get; set; } = new Icons.Regular.Size24.ChevronRight();
+
     /// <summary />
     private string? ClassValue => DefaultClassBuilder
-        .AddClass("fluentcx-color-palette")
+        .AddClass("fluentcx-slideshow")
         .Build();
 
     /// <summary />
